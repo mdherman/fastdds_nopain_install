067db28cc5220a7538c7fb887cfbb57e8bf2ecae
mdherman <martin.hermansen@hotmail.com>
2021-03-19 18:49:25 +0100

    Fixes

--- fastdds_instalation.sh
+++ fastdds_instalation.sh
@@ -2,7 +2,6 @@
 # Works with: foxy, eloquent
 # ------------------------------------
 
-# Prompt user for ...
 
 sudo apt install openjdk-8-jdk -y
 
@@ -12,21 +11,24 @@ cd ~
 curl -s "https://get.sdkman.io" | bash
 source "$HOME/.sdkman/bin/sdkman-init.sh"
 
+mkdir ~/FastRTPS
+cd ~/FastRTPS
+
 git clone https://github.com/eProsima/foonathan_memory_vendor.git
 cd foonathan_memory_vendor
 mkdir build & cd build
 cmake ..
 cmake --build . --target install
 
-git clone --recursive https://github.com/eProsima/Fast-DDS.git -b v2.0.0 ~/FastDDS-2.0.0
-cd ~/FastDDS-2.0.0
+git clone --recursive https://github.com/eProsima/Fast-DDS.git -b v2.0.0 ~/FastRTPS/FastDDS-2.0.0
+cd ~/FastRTPS/FastDDS-2.0.0
 mkdir build && cd build
 
 cmake -DTHIRDPARTY=ON -DSECURITY=ON ..
 make
 sudo make install
 
-git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 ~/Fast-RTPS-Gen \
-    && cd ~/Fast-RTPS-Gen \
-    && ./gradlew assemble \
-    && sudo ./gradlew install
+git clone --recursive https://github.com/eProsima/Fast-DDS-Gen.git -b v1.0.4 ~/FastRTPS/Fast-RTPS-Gen
+cd ~/FastRTPS/Fast-RTPS-Gen
+./gradlew assemble
+sudo ./gradlew install
